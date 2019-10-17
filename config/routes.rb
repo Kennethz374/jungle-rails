e066a6af9c2ac680c080f88bbfb2162fa8814920
Kenneth <sally@sallyde-MacBook-Air.local>
2019-10-17 14:52:05 -0700

    set up sign up, post routes, create controller users, model user, action new and create

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
   end
 
   get 'about/show'
-  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -76,3 +77,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
+
