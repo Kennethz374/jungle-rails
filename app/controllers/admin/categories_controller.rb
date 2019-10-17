e7a8f44465523ba335bd786c63411d2031bb3aea
Kenneth <sally@sallyde-MacBook-Air.local>
2019-10-17 15:39:07 -0700

    implement all feature, move on to polishing some ccs

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+  # http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+  before_filter :authorize
+  
   def index
     @categories = Category.all
   end
